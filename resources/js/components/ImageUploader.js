691e60db784c6fae48ea32c772919af2fe0158c8
Jorge Cortes <jorge.cortes@gmail.com>
2021-07-09 16:17:59 -0700

    Added a simple flash message implementation for file type error

--- resources/js/components/ImageUploader.js
+++ resources/js/components/ImageUploader.js
@@ -8,7 +8,8 @@ class ImageUploader extends React.Component {
         super(props);
         this.state = {
             file: [],
-            images: []
+            images: [],
+            flashMessage: {}
         };
     }
 
@@ -50,13 +51,21 @@ class ImageUploader extends React.Component {
                 'X-CSRF-TOKEN': document.head.querySelector("[name=csrf-token]").content
             },
             body: formData,
-        }).then((response) => response.json())
-            .then(({data}) => component.setState({images: data.images.concat(component.state.images)}))
-            .catch((error) => console.log(error))
-            .finally(() => {
-                component.setState({file: []})
-                NProgress.done();
+        }).then((response) => {
+            response.json().then((data) => {
+                if (response.ok) {
+                    component.setState({images: data.data.images.concat(component.state.images)})
+                } else {
+                    component.setFlashMessage(data);
+                }
             });
+        }).catch((error) => {
+            console.log(error);
+        })
+        .finally(() => {
+            component.setState({file: []})
+            NProgress.done();
+        });
     }
 
     removeImage(index) {
@@ -76,49 +85,72 @@ class ImageUploader extends React.Component {
             .finally(() => NProgress.done());
     }
 
+    setFlashMessage(response) {
+        this.setState({flashMessage: response});
+        setTimeout(() => {
+            this.setState({flashMessage: {}});
+        }, 2000);
+    }
+
     render() {
+        const { flashMessage } = this.state;
+
         return (
-            <div className="container">
-                <div className="row justify-content-center mb-5">
-                    <div className="col-md-8">
-                        <div className="card h-100">
-                            <div className="card-body d-flex align-items-center">
-                                <form onSubmit={this.handleSubmit} className="w-100">
-                                    <div className="form-row align-items-center">
-                                        <div className="col-auto flex-grow-1">
-                                            <input type="file" name="file" onChange={this.handleChange} />
-                                        </div>
-                                        <div className="col-auto">
-                                            <button disabled={!this.state.file.length} className="btn btn-success">Upload Image</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card">
-                            <div className="card-body text-center">
-                                <Dropzone onDrop={acceptedFiles => this.handleDropzone(acceptedFiles)}>
-                                    {({getRootProps, getInputProps}) => (
-                                        <section>
-                                            <div {...getRootProps()}>
-                                                <input {...getInputProps()} />
-                                                <p>Drag 'n' drop some files here, or click to select files</p>
-                                            </div>
-                                        </section>
-                                    )}
-                                </Dropzone>
-                            </div>
-                        </div>
+            <div>
+                {Object.keys(flashMessage).length !== 0 && (
+                <div className="flash-container">
+                    <div className={`flash-body ${flashMessage.status === 'error' ? "flash-danger" : "flash-success"}`}>
+                        {flashMessage.message}
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col">
-                        <div className="grid">
-                            {this.state.images.map((value, index) => {
-                                return <div className="grid__item" key={index}><img src={value} alt={value}/><button className="btn btn-danger" onClick={() => this.removeImage(index)}>Remove</button></div>
-                            })}
+                )}
+                <div className="container">
+                    <div className="row justify-content-center mb-5">
+                        <div className="col-md-8">
+                            <div className="card h-100">
+                                <div className="card-body d-flex align-items-center">
+                                    <form onSubmit={this.handleSubmit} className="w-100">
+                                        <div className="form-row align-items-center">
+                                            <div className="col-auto flex-grow-1">
+                                                <input type="file" name="file" onChange={this.handleChange} accept="image/png"/>
+                                            </div>
+                                            <div className="col-auto">
+                                                <button disabled={!this.state.file.length} className="btn btn-success">Upload Image</button>
+                                            </div>
+                                        </div>
+                                        <div className="form-row">
+                                            <div className="col-auto">
+                                                <small className="form-text text-muted">Supported formats: PNG</small>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="card">
+                                <div className="card-body text-center">
+                                    <Dropzone onDrop={acceptedFiles => this.handleDropzone(acceptedFiles)}>
+                                        {({getRootProps, getInputProps}) => (
+                                            <section>
+                                                <div {...getRootProps()}>
+                                                    <input {...getInputProps()} />
+                                                    <p>Drag 'n' drop some PNG files here, or click to select files</p>
+                                                </div>
+                                            </section>
+                                        )}
+                                    </Dropzone>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <div className="grid">
+                                {this.state.images.map((value, index) => {
+                                    return <div className="grid__item" key={index}><img src={value} alt={value}/><button className="btn btn-danger" onClick={() => this.removeImage(index)}>Remove</button></div>
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
